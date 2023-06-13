cc46cfadf84148fc6d59d13ada5111ca8e0df1fe
Amuro2 <amuro.takahashi@gmail.com>
2023-06-13 22:12:15 +0200

    Initialize Spring Boot Application

---
+++ src/main/java/com/amtak/muselspringapi/MuselspringapiApplication.java
@@ -0,0 +1,13 @@
+package com.amtak.muselspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuselspringapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuselspringapiApplication.class, args);
+	}
+
+}
